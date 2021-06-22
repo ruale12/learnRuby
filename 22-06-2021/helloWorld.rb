00045983830435e16b01d8b81c6b3a78c4aa283e
rubens alexander rangel gomez <rualexander12@gmail.com>
2021-06-22 11:07:50 -0400

    string and nummers IO

--- 22-06-2021/helloWorld.rb
+++ 22-06-2021/helloWorld.rb
@@ -1,7 +1,16 @@
 puts "hello Rubens"
+
 num1 = 20
 num2 = num1.to_f
 puts num2
+
 num1 = 3.1415
 num2 = num1.to_i
 puts num2
+
+name = "rubens"
+puts "hola " + name
+name = "david"
+puts "hola #{ name }"
+#puts num2.methods
+puts "hola #{ name.upcase }, #{ name.downcase },  #{ name.capitalize }"
---
+++ 22-06-2021/strings_and_nummers.rb
@@ -0,0 +1,3 @@
+# encoding: UTF-8
+
+puts "león, corazón"
